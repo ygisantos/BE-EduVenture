be4bfaa5630ba34db4735f8d0c8500afd69f8c1c
Ygi Martin Santos <Ygisantos@gmail.com>
2025-04-18 16:51:58 +0800

    Change Account model to extend Authenticatable for improved authentication handling

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Account extends Model
+class Account extends Authenticatable
 {
     use HasApiTokens;
     protected $fillable = [
